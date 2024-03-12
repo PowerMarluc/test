59f7a0af32ffbda888c1937ac981abcd14b2229c
Ektoplasm <ektoplasm@Ektoplasms-MacBook-Air.local>
2024-03-12 17:27:37 +0100

    Initial commit gh repo clone PowerMarluc/test
    q
    Q

---
+++ javascript.js
@@ -0,0 +1,11 @@
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Vous pouvez ajouter des écouteurs d'événements et d'autres logiques ici
+    const orderButton = document.getElementById('order');
+    orderButton.addEventListener('click', () => {
+        // Logique de commande ou redirection
+        alert('Order button clicked!');
+    });
+});
